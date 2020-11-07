e3d1b8366f70025b7d6786988e73ee1eb5cda556
Lukas Maly <Iam@LukasMaly.NET>
2020-11-07 17:36:22 +0100

    First scripts

---
+++ scripts/create-gcp-vm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcloud compute instances create zbx-fbsd-tsdb --image freebsd-12-2-release-amd64 --image-project=freebsd-org-cloud-dev
---
+++ scripts/install-gcp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+pkg install -y bash
+pkg install -y mc
+pkg install -y joe
+pkg install -y screen
+pkg install -y portupgrade
+pkg install -y postgresql12-server
+pkg install -y postgresql-odbc
+pkg install -y timescaledb
+pkg install -y apache24
+pkg install -y mod_php74
+pkg install -y zabbix5-server
+pkg install -y zabbix5-agent
+pkg install -y zabbix5-frontend
+pkg install -y zabbix5-java
+pkg install -y py-certbot-dns-cloudflare
+# EOF
