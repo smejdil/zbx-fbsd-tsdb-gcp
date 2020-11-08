1c95f6c9ae6d6e1fea922d2f8a83708572ef823d
Lukas Maly <Iam@LukasMaly.NET>
2020-11-08 11:51:50 +0100

    Changes

--- scripts/create-gcp-vm.sh
+++ scripts/create-gcp-vm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Create GCP FreeBSD for Zabbix and TSDB
+# Create GCP VM FreeBSD 12 for Zabbix and TSDB
 #
-# Lukas Maly <Iam@LukasMaly.NET> 7.11.2020
+# Lukas Maly <Iam@LukasMaly.NET> 8.11.2020
 #
 
-IMAGE_FBSD="freebsd-12-2-release-amd64";
+IMAGE_FBSD=`gcloud compute images list --project freebsd-org-cloud-dev --no-standard-images | grep -i 12-2-release-amd64 | awk '{print $1}'`;
 
 gcloud compute instances create zbx-fbsd-tsdb --image ${IMAGE_FBSD} --image-project=freebsd-org-cloud-dev --metadata-from-file startup-script=scripts/install-gcp.sh
 gcloud compute instances add-tags zbx-fbsd-tsdb  --tags=http-server
