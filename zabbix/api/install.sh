aec9226f50c00220ac34c87eb5a089be2321e54e
Lukas Maly <Iam@LukasMaly.NET>
2020-11-08 20:00:52 +0100

    Changes

--- zabbix/api/install.sh
+++ zabbix/api/install.sh
@@ -10,11 +10,17 @@ make install clean
 
 echo "--- Install API example file ---"
 
-INSTALL_DIR='/root/zabbix';
+INSTALL_DIR="/root/zabbix";
 mkdir ${INSTALL_DIR};
 
-install -g wheel -o root -m 700 auth.pl $INSTALL_DIR/
-install -g wheel -o root -m 700 hosts.pl $INSTALL_DIR/
+cd /root/zbx-fbsd-tsdb-gcp/zabbix/api
+install -g wheel -o root -m 700 auth.pl ${INSTALL_DIR}/
+install -g wheel -o root -m 700 hosts.pl ${INSTALL_DIR}/
 
+cd ${INSTALL_DIR}
+./auth.pl
+./hosts.pl
+
+echo "Create zabbix user zbx_probe";
 
 # EOF
